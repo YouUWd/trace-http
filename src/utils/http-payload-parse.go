757036719c2609977315b84c63a91e41827172ff
youyou.dyy <youyou.dyy@alibaba-inc.com>
2019-09-29 14:11:13 +0800

    deal with chunked and no chunked response data

--- src/utils/http-payload-parse.go
+++ src/utils/http-payload-parse.go
@@ -2,13 +2,16 @@ package utils
 
 import (
 	"fmt"
-	"log"
 	"strconv"
 	"strings"
 )
 
-func parseContent(content []byte, request bool) {
-	if len(content) == 0 {
+/**
+only parse the http content
+*/
+func parseContent(content []byte, request bool, chunked bool) {
+	length := len(content)
+	if length == 0 {
 		fmt.Println("Data:")
 		return
 	} else {
@@ -18,41 +21,51 @@ func parseContent(content []byte, request bool) {
 			fmt.Print("Data:")
 		}
 	}
-	cur := 0
-	chunkIndex := 0
-	for i, b := range content {
-		if i < len(content)-1 && b == '\r' && content[i+1] == '\n' {
-			line := string(content[cur:i])
-			if chunkIndex%2 == 0 {
+	if chunked {
+		cur := 0
+		for i := 0; i < length; i++ {
+			if i <= length-1 && content[i] == '\n' {
+				var line string
+				if content[i-1] == '\r' {
+					line = string(content[cur : i-1])
+				} else {
+					line = string(content[cur:i])
+				}
 				length, err := strconv.ParseInt(line, 16, 0)
 				if err != nil {
-					log.Fatal(err)
-				}
-				if length == 0 {
 					break
 				}
-			} else {
-				fmt.Println(line)
+				if length > 0 {
+					fmt.Println(string(content[i+1 : i+1+int(length)]))
+					i = i + 2 + int(length)
+					cur = i
+				}
 			}
-			cur = i + 2
-			chunkIndex++
 		}
+	} else {
+		fmt.Println(string(content))
 	}
-
 }
 
+/**
+parse the whole payload(http)
+*/
 func ParsePayload(payload []byte) {
 	request := false
+	chunked := false
 	cur := 0
 	for i, b := range payload {
-		if i < len(payload)-1 && b == '\r' && payload[i+1] == '\n' {
-			line := string(payload[cur : i+2])
+		if i <= len(payload)-1 && b == '\n' {
+			line := string(payload[cur : i+1])
 			if strings.HasPrefix(line, "Host:") {
 				request = true
 			}
-			cur = i + 2
+			if strings.HasPrefix(line, "Transfer-Encoding: chunked") {
+				chunked = true
+			}
+			cur = i + 1
 			if len(line) == 2 {
-				parseContent(payload[cur:], request)
+				parseContent(payload[cur:], request, chunked)
 				break
 			} else {
 				fmt.Print(line)
